d7e9f05b9f4b630b4d42ecb4cb701d433fddd9c7
juahcheon <132863519+juahcheon@users.noreply.github.com>
2024-09-28 23:23:06 +0900

    .

--- react-a_z/react-nexflix-end/src/Pages/SearchPage/index.js
+++ react-a_z/react-nexflix-end/src/Pages/SearchPage/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import axios from '../../api/axios';
 import './SearchPage.css';
+import { useDebounce } from '../../hooks/useDebounce';
+
 
 export default function SearchPage() {
 
@@ -9,18 +11,20 @@ export default function SearchPage() {
   const [searchResults, setSearchResults] = useState([]);
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
-  }
+  };
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if(searchTerm) {
-      fetchSearchMovie();
+    if(debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
   
   const fetchSearchMovie = async (searchTerm) => {
+    console.log('searchTerm: ' + searchTerm);
     try {
       const request = await axios.get(
         `/search/multi?include_adult=false&query=${searchTerm}`
@@ -39,7 +43,7 @@ export default function SearchPage() {
             const movieImageUrl = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
             return (
               <div className='movie' key={movie.id}>
-                <div className='movie__column-poster'>
+                <div onClick={() => navigate(`/${movie.id}`)} className='movie__column-poster'>
                   <img 
                     src={movieImageUrl}
                     alt='movie'
@@ -55,7 +59,7 @@ export default function SearchPage() {
       <section className='no-results'>
         <div className='no-results__text'>
           <p>
-            찾고자 하는 검색어 '{searchTerm}'에 맞는 영화가 없습니다.
+            찾고자 하는 검색어 '{debouncedSearchTerm}'에 맞는 영화가 없습니다.
           </p>
         </div>
       </section>
