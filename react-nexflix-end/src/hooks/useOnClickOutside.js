15e5d03621aaf2c2c875bf85fb6946c4219012fb
juahcheon <132863519+juahcheon@users.noreply.github.com>
2024-10-01 17:19:29 +0900

    .

---
+++ react-nexflix-end/src/hooks/useOnClickOutside.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+
+    const listener = (event) => {
+      if(!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+  
+    return () => {
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+    }
+  }, [ref, handler]);
+  
+}
+
+export default useOnClickOutside;
