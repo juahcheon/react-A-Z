9a52524f0ccca79a89a53930b2a72bceaca4a06d
juahcheon <132863519+juahcheon@users.noreply.github.com>
2024-10-25 22:10:19 +0900

    .

--- react-a_z/nextjs-cathing/nextjs-15/src/app/about/page.tsx
+++ react-a_z/nextjs-cathing/nextjs-15/src/app/about/page.tsx
@@ -1,8 +1,10 @@
+export const dynamic = "force-static";
+
 export default async function About() {
   return (
     <div>
       About
       <h2>{new Date().toISOString()}</h2>
     </div>
-  )
+  );
 }
