cd31fbd4fc0232b883457b1e36290fb89de1b182
juahcheon <132863519+juahcheon@users.noreply.github.com>
2024-10-31 23:23:18 +0900

    .

--- react-a_z/react-redux-app/src/App.js
+++ react-a_z/react-redux-app/src/App.js
@@ -1,16 +1,70 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { useState } from 'react';
-import ChildComponentA from './ChildComponentA';
+import axios from 'axios';
 
 function App() {
+
+  const counter =  useSelector((state) => state.counter);
+  const todos = useSelector((state) => state.todos);
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  }
   
-  const [inputValue, setInputValue] = useState("A");
+  const [todoValue, setTodoValue] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', text: todoValue });
+  }
+
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+  }
+
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  }
 
   return (
     <div className="App">
-      APP Component<br />
-      {inputValue}
-      <ChildComponentA inputValue={inputValue} setInputValue={setInputValue} />
+      <div>
+        <ul>
+          {posts.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul>
+      </div>
+      
+      <div>
+        <ul>
+          {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text' 
+            value={todoValue} 
+            onChange={(e) => setTodoValue(e.target.value)} />
+            <input type='submit' />
+        </form>
+      </div>
+
+      <div>
+        Clicked: { counter } times
+        {" "}
+        <button onClick={handleIncrement}>
+          +
+        </button>
+        {" "}
+        <button onClick={handleDecrement}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
--- react-a_z/react-redux-app/src/index.js
+++ react-a_z/react-redux-app/src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { loggerMiddleware } from './middleware';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const middleware = applyMiddleware(loggerMiddleware);
+
+const store = createStore(rootReducer, middleware);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App
+        // onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        // onDecrement={() => store.dispatch({ type: 'DECREMENT' })}  
+      />
+    </Provider>
+    
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
